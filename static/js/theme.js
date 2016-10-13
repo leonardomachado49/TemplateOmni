02bef052c3483b876166643a16615f0e2fd08637
leonardotexiera49 <leonardozb3@gmail.com>
2016-10-13 16:48:08 -0300

    Template Omni

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
-
+  $(document).ready(function(){
 
       $(".button-collapse").sideNav();
 
-});
+      $(document).ready(function(){
+        $('.carousel').carousel();
+      });
+      $(document).ready(function(){
+        $('.slider').slider({full_width: true});
+      });
+      $(document).ready(function(){
+        $('.parallax').parallax();
+      });
+
+  });
